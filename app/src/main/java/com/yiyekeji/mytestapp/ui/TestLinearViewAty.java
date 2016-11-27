e667ebe6d2b97e6490fd02bba4d5eb0ddae47d4a
lxllxllxs <77478516@qq.com>
2016-11-27 12:43:56 +0800

    hellochart

--- app/src/main/java/com/yiyekeji/mytestapp/ui/TestLinearViewAty.java
+++ app/src/main/java/com/yiyekeji/mytestapp/ui/TestLinearViewAty.java
@@ -1,22 +1,50 @@
 package com.yiyekeji.mytestapp.ui;
 
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.yiyekeji.mytestapp.R;
 import com.yiyekeji.mytestapp.ui.base.BaseActivity;
 import com.yiyekeji.mytestapp.widget.LinearView;
+
+import java.util.LinkedHashMap;
+import java.util.Random;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2016/11/26.
  */
 public class TestLinearViewAty extends BaseActivity {
+    @InjectView(R.id.lv_linear_chart)
+    LinearView lvLinearChart;
+    @InjectView(R.id.btn_set)
+    Button btnSet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new LinearView(this));
+        setContentView(R.layout.activity_linearview);
+        ButterKnife.inject(this);
     }
 
     @Override
     public void initView() {
 
+    }
+
+    @OnClick(R.id.btn_set)
+    public void onClick() {
+        Random random = new Random();
+        int max=720;
+        int min=100;
+        LinkedHashMap<Integer, Integer> hashMap = new LinkedHashMap<>();
+        for (int i=0;i<20;i++){
+            int s=random.nextInt(max)%(max-min+1) + min;
+            hashMap.put(i*35,random.nextInt(max)%(max-min+1) + min);
+        }
+        lvLinearChart.setLinkHashMap(hashMap);
     }
 }
--- app/src/main/java/com/yiyekeji/mytestapp/widget/LinearView.java
+++ app/src/main/java/com/yiyekeji/mytestapp/widget/LinearView.java
@@ -27,46 +27,70 @@ public class LinearView extends View {
     public LinearView(Context context) {
         super(context);
         this.context = context;
-        initPaint();
+        init();
     }
     public LinearView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
-        initPaint();
+        init();
     }
     public LinearView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.context = context;
-        initPaint();
+        init();
     }
-
+    private int color_black,color_orange,color_gray;
     private Paint mPaint;
     private  Path path;
     int screenHeight,screenWeight;
-    private void initPaint(){
+    private Map<Integer, Integer> coordinateMap = new LinkedHashMap<>();
+    int XPoint=60;
+    int YLength=700;
+    int YScale=600/10;
+    int XLength=700;
+    int XScale=600/10;
+
+    private void init(){
+        color_black=ContextCompat.getColor(context,R.color.gray_black);
+        color_orange=ContextCompat.getColor(context,R.color.orange);
+        color_gray=ContextCompat.getColor(context,R.color.gray);
+
+
         screenWeight= ScreenUtils.getScreenSize(context,true)[0];
         screenHeight=ScreenUtils.getScreenSize(context,true)[1];
+
+        XLength=screenWeight;
+        YLength=screenWeight;
+
+        XScale=XLength/10;
+        YScale=YLength/10;
+
         LogUtil.d("sdasd",screenHeight+"=="+screenHeight);
-        CornerPathEffect cornerPathEffect = new CornerPathEffect(200);
+        CornerPathEffect cornerPathEffect = new CornerPathEffect(50);
         mPaint=new Paint();
-        mPaint.setColor(ContextCompat.getColor(context, R.color.orange));
+        mPaint.setColor(color_gray);
         mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.STROKE);  //画笔风格
         mPaint.setTextSize(26);
-//        mPaint.setStrokeJoin(Paint.Join.ROUND);
-        mPaint.setStrokeWidth(3);           //画笔粗细
-//        mPaint.setPathEffect(cornerPathEffect);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeWidth(2);           //画笔粗细
+        mPaint.setTextAlign(Paint.Align.CENTER);
+        mPaint.setPathEffect(cornerPathEffect);
         path= new Path();
-        path.moveTo(50,50);
+/*        path.moveTo(50,50);
         path.lineTo(60,150);
 
         path.lineTo(70,150);
         path.lineTo(140,300);
         path.lineTo(210,10);
         path.lineTo(250,50);
-        path.lineTo(290,150);
+        path.lineTo(290,150);*/
     }
 
+    public  void setLinkHashMap(LinkedHashMap<Integer,Integer> map){
+        this.coordinateMap=map;
+        invalidate();
+    }
 
     /**
      * 画布的宽为屏宽的5/6,高为屏宽的5/8
@@ -75,81 +99,98 @@ public class LinearView extends View {
      */
     @Override
     protected void onDraw(Canvas canvas) {
-        int paddingTop=(screenHeight-screenWeight*5/8)/2;
         canvas.drawColor(Color.WHITE);
-     /*   canvas.drawRect(screenWeight/12, paddingTop,
-                screenWeight*11/12,paddingTop+screenWeight*5/8, mPaint);*/
-//        canvas.drawPath(path, mPaint);
         drawYLine(canvas);
         drawXLine(canvas);
         drawCircle(canvas);
     }
-
-    int XPoint=50;
-    int YPoint=30;
-    int YLength=1000;
-    int YScale=900/10;
     private void drawYLine(Canvas canvas){
         String[] YLabel={"a","b","c","d","e",
                         "f","g","h","i","j","k"};
-
-        //画Y轴
-        canvas.drawLine(XPoint, YLength, XPoint, YPoint, mPaint);
-
-        //Y轴箭头
-        canvas.drawLine(XPoint, YPoint, XPoint - 10, YPoint + 30, mPaint);  //箭头
-        canvas.drawLine(XPoint, YPoint, XPoint + 10, YPoint + 30 ,mPaint);
-
         //添加刻度和文字
         for(int i=0; i  < 10; i++) {
-            canvas.drawLine(XPoint, YLength-i*YScale, XPoint + 20, YLength-i*YScale, mPaint);  //刻度
+            mPaint.setColor(color_gray);
+            canvas.drawLine(XPoint, YLength-i*YScale,XLength, YLength-i*YScale, mPaint);  //刻度
+            mPaint.setColor(color_black);
             canvas.drawText(YLabel[i], XPoint-20, YLength - i * YScale, mPaint);//文字
         }
     }
-
-    int XLength=700;
-    int XScale=600/10;
     private void drawXLine(Canvas canvas){
-        String[] XLabel={"1","2","3","4","5",
+        String[] XLabel={"0","1","2","3","4","5",
                 "6","7","8","9","10","k"};
-
+        mPaint.setColor(ContextCompat.getColor(context,R.color.gray));
         //画X轴
-        canvas.drawLine(XPoint, YLength, XLength, YLength, mPaint);
-
-        //X轴箭头
-        canvas.drawLine(XLength, YLength, XLength - 30, YLength - 10, mPaint);  //箭头
-        canvas.drawLine(XLength, YLength, XLength - 30, YLength +10 ,mPaint);
-
+        canvas.drawLine(XPoint, YLength, XLength+XPoint, YLength, mPaint);
         //添加刻度和文字
         for(int i=0; i  < 10; i++) {
-            canvas.drawLine(XPoint+i*XScale,YLength, XPoint+i*XScale, YLength-10, mPaint);  //刻度
-            canvas.drawText(XLabel[i], XPoint+i*XScale, YLength+30, mPaint);//文字
+            mPaint.setColor(color_black);
+            canvas.drawLine(XPoint+i*XScale,YLength, XPoint+i*XScale, YLength-30, mPaint);  //刻度
+            canvas.drawText(XLabel[i], XPoint+i*XScale, YLength+50, mPaint);//文字
         }
     }
 
     /**
-     * 把（50,1000）当成原坐标
+     * 把（50,700）当成原坐标
+     * 闭合坐标（750，700）
      */
 
-    private Map<Integer, Integer> coordinateMap = new LinkedHashMap<>();
     private  void drawCircle(Canvas canvas){
-        coordinateMap.put(60,90);
-        coordinateMap.put(120,270);
-        coordinateMap.put(180,360);
-        coordinateMap.put(240,180);
-        coordinateMap.put(300,90);
-        coordinateMap.put(360,810);
-
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(6);
+        mPaint.setColor(ContextCompat.getColor(context,R.color.orange));
         Path path=new Path();
-        path.moveTo(XPoint, YLength);
+
+        int key=0,value=0;
+        boolean isFirst = true;
         Iterator<Map.Entry<Integer,Integer>> iterator=coordinateMap.entrySet().iterator();
+        int firstX=0,firstY=0;
         while (iterator.hasNext()) {
             Map.Entry entry = (Map.Entry) iterator.next();
-            int key = (int)entry.getKey();
-            int value = (int)entry.getValue();
-            canvas.drawCircle(key,YLength-value,3,mPaint);
-            path.lineTo(key,YLength-value);
+            key= (int)entry.getKey();
+            value = (int)entry.getValue();
+            if (isFirst) {
+                isFirst = false;
+                firstX = key+XPoint;
+                firstY=YLength-value;
+                //画首线 用于填充完成封闭
+                mPaint.setColor(color_orange);
+                path.moveTo(XPoint, YLength);
+            }
+            path.lineTo(key+XPoint,YLength-value);
+//            canvas.drawCircle(key+XPoint,YLength-value,6,mPaint);
         }
+        //第一期也要到底 才能使填充完美
+        path.lineTo(XLength,YLength);
         canvas.drawPath(path, mPaint);
+        //覆盖画一条起始线 改色
+        Path path0=new Path();
+        path0.moveTo(XPoint,YLength);
+        path0.lineTo(firstX,firstY);
+        mPaint.setColor(color_gray);
+        canvas.drawPath(path0,mPaint);
+        //覆盖画一条完结线 改色
+        Path path2=new Path();
+        path2.moveTo(key+XPoint,YLength-value);
+        path2.lineTo(XLength,YLength);
+        canvas.drawPath(path2,mPaint);
+        //换色填充 应改为渐变色
+        path.close();
+        mPaint.setColor(ContextCompat.getColor(context,R.color.gray));
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawPath(path, mPaint);
+    }
+
+
+    private void setLinerMap(){
+        coordinateMap.put(0,0);
+        coordinateMap.put(1*XScale,1*YScale);
+        coordinateMap.put(2*XScale,2*YScale);
+        coordinateMap.put(3*XScale,3*YScale);
+        coordinateMap.put(4*XScale,4*YScale);
+        coordinateMap.put(5*XScale,5*YScale);
+        coordinateMap.put(6*XScale,6*YScale);
+        coordinateMap.put(7*XScale,7*YScale);
+        coordinateMap.put(8*XScale,8*YScale);
+        coordinateMap.put(9*XScale,9*YScale);
     }
 }
